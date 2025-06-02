d582971cc6ed1c1c131c5ebfa0b8387bb60b87eb
zaid-hm <135333885+zaid-hm@users.noreply.github.com>
2025-06-03 00:21:49 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -34,8 +34,8 @@ FRAMERATE = "30"
 # The camera's distance from the nozzle.
 # This tells the recording code how to center the line within the camera's field of view.
 # The offsets are in mm.
-CAMERA_OFFSET_X = 28
-CAMERA_OFFSET_Y = 50.2
+CAMERA_OFFSET_X = 12.324
+CAMERA_OFFSET_Y = 67.224
 
 # This is the height where the camera and laser are in focus.
 LASER_FOCUS_HEIGHT = 13.860
@@ -61,7 +61,7 @@ FFMPEG_STOP_DELAY = 0.6
 # Only edit this if you need to.
 Z_HOP_HEIGHT = 0.75
 LAYER_HEIGHT = 0.25
-RETRACTION_DISTANCE = 0.5
+RETRACTION_DISTANCE = 0.4
 EXTRUSION_DISTANCE_PER_MM = 0.045899
 BOUNDING_BOX_LINE_WIDTH = 0.4 # May need adjustment. 
 
